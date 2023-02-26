1c6e92a56cf49590837d096ecd5228aef55f8e85
ifuture <ifuture@ifuturedeMacBook-Pro.local>
2023-02-26 15:54:57 +0800

    feat: update ui

--- packages/crossview-ui/index.ts
+++ packages/crossview-ui/index.ts
@@ -1,4 +1,3 @@
 export { CrossViewUIParagraph } from './src/components/paragraph/ui-paragraph';
 export { CrossViewUIAbstract } from './src/components/abstract/ui-abstract';
 export { CrossViewUIImage } from './src/components/image/ui-image';
-export { CrossViewUIVideo } from './src/components/video/ui-video';
