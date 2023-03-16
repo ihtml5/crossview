d9a91b1609a0ed50b866ae2fa8d24c084d0e3461
ifuture <ifuture@ifuturedeMacBook-Pro.local>
2023-03-16 09:39:48 +0800

    feat: add vue project

---
+++ packages/crossview-vue/src/views/CrossView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <kwr-ui-interactive></kwr-ui-interactive>
+    <kwr-ui-protable></kwr-ui-protable>
+    <crossview-ui-image src="https://inews.gtimg.com/newsapp_bt/0/13843563168/641"></crossview-ui-image>
+  </div>
+</template>
+<script lang="ts" setup>
+import 'crossview-react/dist/cfc';
+import 'crossview-react/dist/protable';
+import 'crossview-ui/components/heading';
+import 'crossview-ui/components/abstract';
+import 'crossview-ui/components/image';
+</script>
---
+++ packages/crossview-vue/vite.config.ts
@@ -0,0 +1,34 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.js'), // 设置入口文件
+      name: 'nf-tool', // 起个名字，安装、引入用
+      fileName: (format) => `nf-tool.${format}.js` // 打包后的文件名
+    },
+    sourcemap: true, // 输出.map文件
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+      output: {
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
